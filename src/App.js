3aaf021e8e42f08baaba977ce6dd78a65943cb63
Jinyoung Ahn <48481448+zeroaan@users.noreply.github.com>
2020-10-02 22:40:06 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [data, setData] = useState(0);
+  const plusData = () => {
+    setData((prevData) => {
+      return prevData + 1;
+    });
+  };
+  const minusData = () => {
+    setData((prevData) => {
+      return prevData - 1;
+    });
+  };
+  const resetData = () => {
+    setData(0);
+  };
   return (
-	<div className="container">
-		<h2>hello</h2>
-	</div>;
-  )
+    <div className="container">
+      <h2>hello</h2>
+      <h4>{data}</h4>
+      <button onClick={plusData}>Plus</button>
+      <button onClick={minusData}>Minus</button>
+      <button onClick={resetData}>Reset</button>
+    </div>
+  );
 }
 
 export default App;
