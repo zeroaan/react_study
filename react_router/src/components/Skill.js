a1e2fa0947ddc461ea700a1a13172d6e42896d65
Jinyoung Ahn <48481448+zeroaan@users.noreply.github.com>
2021-01-10 16:47:10 +0900

    react-router

--- react_router/src/components/Skill.js
+++ react_router/src/components/Skill.js
@@ -1,9 +1,19 @@
 import React from "react"
+import { Link } from "react-router-dom"
+
+import { SkillData } from "data/SkillData"
+
+import "index.scss"
 
 const Skill = () => {
   return (
     <>
       <div>Skill</div>
+      {SkillData.map((v) => (
+        <Link className="skill" key={v.id} to={`/skill/${v.id}`}>
+          {v.name}
+        </Link>
+      ))}
     </>
   )
 }
