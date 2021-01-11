15a79645a974a55e277da738cb9de8e1b9d367a7
Jinyoung Ahn <48481448+zeroaan@users.noreply.github.com>
2021-01-11 20:04:35 +0900

    scss

--- react_scss/src/App.js
+++ react_scss/src/App.js
@@ -3,7 +3,14 @@ import React from "react"
 const App = () => {
   return (
     <>
-      <div>App</div>
+      <div className="app">
+        <h1>one</h1>
+        <div>
+          <h1>two</h1>
+        </div>
+      </div>
+      <h1>three</h1>
+      <h1>four</h1>
     </>
   )
 }
